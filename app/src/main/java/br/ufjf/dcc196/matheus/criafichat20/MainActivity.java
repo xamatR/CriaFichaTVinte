b84dceb3e7a56613b3d03d87128d406372080434
xamatR <77069479+xamatR@users.noreply.github.com>
2021-09-10 15:27:17 -0300

    Cria a classe de Personagem.java mas ainda n implementa ela completamente , pois estou pensando na melhor abordagem para finalizar ela.

--- app/src/main/java/br/ufjf/dcc196/matheus/criafichat20/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/matheus/criafichat20/MainActivity.java
@@ -1,14 +1,20 @@
 package br.ufjf.dcc196.matheus.criafichat20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonCria;
+    private RecyclerView recyclerViewFichas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewFichas = findViewById(R.id.recyclerViewFichas);
     }
 }
---
+++ app/src/main/java/br/ufjf/dcc196/matheus/criafichat20/Personagem.java
@@ -0,0 +1,28 @@
+package br.ufjf.dcc196.matheus.criafichat20;
+
+public class Personagem {
+    private String nome;
+    private String classe;
+    private String origem;
+    private String divindade;
+    private int nivel;
+    private int pontosvida;
+    private int pontosMana;
+    private int força;
+    private int destreza;
+    private int constituição;
+    private int inteligencia;
+    private int sabedoria;
+    private int Carisma;
+
+    // Modificadores de atributo
+    private int modForça;
+    private int modDestreza;
+    private int modConstituição;
+    private int modInteligencia;
+    private int modSabedoria;
+    private int modCarisma;
+
+
+
+}
