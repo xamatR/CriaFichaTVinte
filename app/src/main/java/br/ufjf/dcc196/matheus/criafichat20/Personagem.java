78449bd9daca07868e65e92c7204b85153db7fec
xamatR <77069479+xamatR@users.noreply.github.com>
2021-09-10 21:36:04 -0300

    Adiciona contstrutor a classe de Personagem.java

--- app/src/main/java/br/ufjf/dcc196/matheus/criafichat20/Personagem.java
+++ app/src/main/java/br/ufjf/dcc196/matheus/criafichat20/Personagem.java
@@ -1,6 +1,8 @@
 package br.ufjf.dcc196.matheus.criafichat20;
 
 public class Personagem {
+    static final private int[] valorModificadores = {-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10};
+
     private String nome;
     private String classe;
     private String origem;
@@ -8,21 +10,788 @@ public class Personagem {
     private int nivel;
     private int pontosvida;
     private int pontosMana;
-    private int força;
+    private int forca;
     private int destreza;
     private int constituição;
     private int inteligencia;
     private int sabedoria;
-    private int Carisma;
+    private int carisma;
 
     // Modificadores de atributo
-    private int modForça;
+    private int modForca;
     private int modDestreza;
     private int modConstituição;
     private int modInteligencia;
     private int modSabedoria;
     private int modCarisma;
 
+/*
+    //pericias
+    private int acrobacia;
+    private int adestramento;
+    private int atletismo;
+    private int atuacao;
+    private int cavalgar;
+    private int conhecimento;
+    private int cura;
+    private int diplomacia;
+    private int enganacao;
+    private int fortitude;
+    private int furtividade;
+    private int guerra;
+    private int iniciativa;
+    private int intimidacao;
+    private int intuicao;
+    private int investigacao;
+    private int jogatina;
+    private int ladinagem;
+    private int luta;
+    private int misticismo;
+    private int nobreza;
+    private int oficio;
+    private int percepcao;
+    private int pilotagem;
+    private int pontaria;
+    private int reflexos;
+    private int religiao;
+    private int sobrevivencia;
+    private int vontade;
+
+    //treinamento em pericia
+    private boolean tAcrobacia;
+    private boolean tAdestramento;
+    private boolean tAtletismo;
+    private boolean tAtuacao;
+    private boolean tCavalgar;
+    private boolean tConhecimento;
+    private boolean tCura;
+    private boolean tDiplomacia;
+    private boolean tEnganação;
+    private boolean tFortitude;
+    private boolean tFurtividade;
+    private boolean tGuerra;
+    private boolean tIniciativa;
+    private boolean tIntimidacao;
+    private boolean tIntuicao;
+    private boolean tInvestigacao;
+    private boolean tJogatina;
+    private boolean tLadinagem;
+    private boolean tLuta;
+    private boolean tMisticismo;
+    private boolean tNobreza;
+    private boolean tOficio;
+    private boolean tPercepcao;
+    private boolean tPilotagem;
+    private boolean tPontaria;
+    private boolean tReflexos;
+    private boolean tReligiao;
+    private boolean tSobrevivencia;
+    private boolean tVontade;
+*/
+    public Personagem(String nome, String classe, String origem, String divindade, int nivel,
+                      int pontosvida, int pontosMana, int forca, int destreza, int constituição,
+                      int inteligencia, int sabedoria, int carisma) {
+        this.nome = nome;
+        this.classe = classe;
+        this.origem = origem;
+        this.divindade = divindade;
+        this.nivel = nivel;
+        this.pontosvida = pontosvida;
+        this.pontosMana = pontosMana;
+        this.forca = forca;
+        this.destreza = destreza;
+        this.constituição = constituição;
+        this.inteligencia = inteligencia;
+        this.sabedoria = sabedoria;
+        this.carisma = carisma;
+        this.modForca = calculaModificador(forca);
+        this.modDestreza = calculaModificador(destreza);
+        this.modConstituição = calculaModificador(constituição);
+        this.modInteligencia = calculaModificador(inteligencia);
+        this.modSabedoria = calculaModificador(sabedoria);
+        this.modCarisma = calculaModificador(carisma);
+    }
+/*
+    protected void setPericias(){
+       this.acrobacia = setadorDePericia(istAcrobacia());
+       this.adestramento = setadorDePericia(istAdestramento());
+       this.atletismo = setadorDePericia(istAtletismo());
+       this.atuacao = setadorDePericia(istAtuacao());
+       this.cavalgar = setadorDePericia(istCavalgar());
+       this.conhecimento = setadorDePericia(istConhecimento());
+       this.cura = setadorDePericia(istCura());
+       this.diplomacia = setadorDePericia(istDiplomacia());
+       this.enganacao = setadorDePericia(istEnganação());
+       this.fortitude = setadorDePericia(istFortitude());
+       this.furtividade = setadorDePericia(istFurtividade());
+       this.guerra = setadorDePericia(istGuerra());
+       this.iniciativa = setadorDePericia(istIniciativa());
+       this.intimidacao = setadorDePericia(istIntimidacao());
+       this.intuicao = setadorDePericia(istIntuicao());
+       this.investigacao = setadorDePericia(istInvestigacao());
+       this.jogatina = setadorDePericia(istJogatina());
+       this.ladinagem = setadorDePericia(istLadinagem());
+       this.luta = setadorDePericia(istLuta());
+       this.misticismo = setadorDePericia(istMisticismo());
+       this.nobreza = setadorDePericia(istNobreza());
+       this.oficio = setadorDePericia(istOficio());
+       this.percepcao = setadorDePericia(istPercepcao());
+       this.pilotagem = setadorDePericia(istPilotagem());
+       this.pontaria = setadorDePericia(istPontaria());
+       this.reflexos = setadorDePericia(istReflexos());
+       this.religiao = setadorDePericia(istReligiao());
+       this.sobrevivencia = setadorDePericia(istSobrevivencia());
+       this.vontade = setadorDePericia(istVontade());
+
+    }
+
+    protected int setadorDePericia(boolean treinada){
+        int pericia=0;
+        if(treinada && this.nivel%2==0) {
+            return pericia = (nivel/2)+2;
+        } else {
+            return pericia = (nivel/2)+1;
+        } if (treinada ){
+            return pericia= (nivel/2)-1;
+        }
+    }
+*/
 
 
+    protected int calculaModificador(int atributo){
+        int modficador=valorModificadores[atributo+1];
+        return modficador;
+    };
+
+
+
+    public static int[] getValorModificadores() {
+        return valorModificadores;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDivindade() {
+        return divindade;
+    }
+
+    public void setDivindade(String divindade) {
+        this.divindade = divindade;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getPontosvida() {
+        return pontosvida;
+    }
+
+    public void setPontosvida(int pontosvida) {
+        this.pontosvida = pontosvida;
+    }
+
+    public int getPontosMana() {
+        return pontosMana;
+    }
+
+    public void setPontosMana(int pontosMana) {
+        this.pontosMana = pontosMana;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public int getConstituição() {
+        return constituição;
+    }
+
+    public void setConstituição(int constituição) {
+        this.constituição = constituição;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getSabedoria() {
+        return sabedoria;
+    }
+
+    public void setSabedoria(int sabedoria) {
+        this.sabedoria = sabedoria;
+    }
+
+    public int getCarisma() {
+        return carisma;
+    }
+
+    public void setCarisma(int carisma) {
+        this.carisma = carisma;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    public int getModForca() {
+        return modForca;
+    }
+
+    public void setModForca(int modForca) {
+        this.modForca = modForca;
+    }
+
+    public int getModDestreza() {
+        return modDestreza;
+    }
+
+    public void setModDestreza(int modDestreza) {
+        this.modDestreza = modDestreza;
+    }
+
+    public int getModConstituição() {
+        return modConstituição;
+    }
+
+    public void setModConstituição(int modConstituição) {
+        this.modConstituição = modConstituição;
+    }
+
+    public int getModInteligencia() {
+        return modInteligencia;
+    }
+
+    public void setModInteligencia(int modInteligencia) {
+        this.modInteligencia = modInteligencia;
+    }
+
+    public int getModSabedoria() {
+        return modSabedoria;
+    }
+
+    public void setModSabedoria(int modSabedoria) {
+        this.modSabedoria = modSabedoria;
+    }
+
+    public int getModCarisma() {
+        return modCarisma;
+    }
+
+    public void setModCarisma(int modCarisma) {
+        this.modCarisma = modCarisma;
+    }
+/*
+    public int getAcrobacia() {
+        return acrobacia;
+    }
+
+    public void settAcrobacia(int tAcrobacia) {
+        this.acrobacia = tAcrobacia;
+    }
+
+    public int getAdestramento() {
+        return adestramento;
+    }
+
+    public void settAdestramento(int tAdestramento) {
+        this.adestramento = tAdestramento;
+    }
+
+    public int getAtletismo() {
+        return atletismo;
+    }
+
+    public void settAtletismo(int tAtletismo) {
+        this.atletismo = tAtletismo;
+    }
+
+    public int getAtuacao() {
+        return atuacao;
+    }
+
+    public void settAtuacao(int tAtuacao) {
+        this.atuacao = tAtuacao;
+    }
+
+    public int getCavalgar() {
+        return cavalgar;
+    }
+
+    public void settCavalgar(int tCavalgar) {
+        this.cavalgar = tCavalgar;
+    }
+
+    public int getConhecimento() {
+        return conhecimento;
+    }
+
+    public void settConhecimento(int tConhecimento) {
+        this.conhecimento = tConhecimento;
+    }
+
+    public int getCura() {
+        return cura;
+    }
+
+    public void settCura(int tCura) {
+        this.cura = tCura;
+    }
+
+    public int getDiplomacia() {
+        return diplomacia;
+    }
+
+    public void settDiplomacia(int tDiplomacia) {
+        this.diplomacia = tDiplomacia;
+    }
+
+    public int getEnganacao() {
+        return enganacao;
+    }
+
+    public void settEnganação(int tEnganação) {
+        this.enganacao = tEnganação;
+    }
+
+    public int getFortitude() {
+        return fortitude;
+    }
+
+    public void settFortitude(int tFortitude) {
+        this.fortitude = tFortitude;
+    }
+
+    public int getFurtividade() {
+        return furtividade;
+    }
+
+    public void settFurtividade(int tFurtividade) {
+        this.furtividade = tFurtividade;
+    }
+
+    public int getGuerra() {
+        return guerra;
+    }
+
+    public void settGuerra(int tGuerra) {
+        this.guerra = tGuerra;
+    }
+
+    public int getIniciativa() {
+        return iniciativa;
+    }
+
+    public void settIniciativa(int tIniciativa) {
+        this.iniciativa = tIniciativa;
+    }
+
+    public int getIntimidacao() {
+        return intimidacao;
+    }
+
+    public void settIntimidacao(int tIntimidacao) {
+        this.intimidacao = tIntimidacao;
+    }
+
+    public int getIntuicao() {
+        return intuicao;
+    }
+
+    public void settIntuicao(int tIntuicao) {
+        this.intuicao = tIntuicao;
+    }
+
+    public int getInvestigacao() {
+        return investigacao;
+    }
+
+    public void settInvestigacao(int tInvestigacao) {
+        this.investigacao = tInvestigacao;
+    }
+
+    public int getJogatina() {
+        return jogatina;
+    }
+
+    public void settJogatina(int tJogatina) {
+        this.jogatina = tJogatina;
+    }
+
+    public int getLadinagem() {
+        return ladinagem;
+    }
+
+    public void settLadinagem(int tLadinagem) {
+        this.ladinagem = tLadinagem;
+    }
+
+    public int getLuta() {
+        return luta;
+    }
+
+    public void settLuta(int tLuta) {
+        this.luta = tLuta;
+    }
+
+    public int getMisticismo() {
+        return misticismo;
+    }
+
+    public void settMisticismo(int tMisticismo) {
+        this.misticismo = tMisticismo;
+    }
+
+    public int getNobreza() {
+        return nobreza;
+    }
+
+    public void settNobreza(int tNobreza) {
+        this.nobreza = tNobreza;
+    }
+
+    public int getOficio() {
+        return oficio;
+    }
+
+    public void settOficio(int tOficio) {
+        this.oficio = tOficio;
+    }
+
+    public int getPercepcao() {
+        return percepcao;
+    }
+
+    public void setPercepcao(int percepcao) {
+        this.percepcao = percepcao;
+    }
+
+    public int getPilotagem() {
+        return pilotagem;
+    }
+
+    public void settPilotagem(int tPilotagem) {
+        this.pilotagem = tPilotagem;
+    }
+
+    public int getPontaria() {
+        return pontaria;
+    }
+
+    public void settPontaria(int tPontaria) {
+        this.pontaria = tPontaria;
+    }
+
+    public int getReflexos() {
+        return reflexos;
+    }
+
+    public void settReflexos(int tReflexos) {
+        this.reflexos = tReflexos;
+    }
+
+    public int getReligiao() {
+        return religiao;
+    }
+
+    public void setReligiao(int religiao) {
+        this.religiao = religiao;
+    }
+
+    public int getSobrevivencia() {
+        return sobrevivencia;
+    }
+
+    public void settSobrevivencia(int tSobrevivencia) {
+        this.sobrevivencia = tSobrevivencia;
+    }
+
+    public int getVontade() {
+        return vontade;
+    }
+
+    public void settVontade(int tVontade) {
+        this.vontade = tVontade;
+    }
+
+    public boolean istAcrobacia() {
+        return tAcrobacia;
+    }
+
+    public void setAcrobacia(boolean acrobacia) {
+        tAcrobacia = acrobacia;
+    }
+
+    public boolean istAdestramento() {
+        return tAdestramento;
+    }
+
+    public void setAdestramento(boolean adestramento) {
+        tAdestramento = adestramento;
+    }
+
+    public boolean istAtletismo() {
+        return tAtletismo;
+    }
+
+    public void setAtletismo(boolean atletismo) {
+        tAtletismo = atletismo;
+    }
+
+    public boolean istAtuacao() {
+        return tAtuacao;
+    }
+
+    public void setAtuacao(boolean atuacao) {
+        tAtuacao = atuacao;
+    }
+
+    public boolean istCavalgar() {
+        return tCavalgar;
+    }
+
+    public void setCavalgar(boolean cavalgar) {
+        tCavalgar = cavalgar;
+    }
+
+    public boolean istConhecimento() {
+        return tConhecimento;
+    }
+
+    public void setConhecimento(boolean conhecimento) {
+        tConhecimento = conhecimento;
+    }
+
+    public boolean istCura() {
+        return tCura;
+    }
+
+    public void setCura(boolean cura) {
+        tCura = cura;
+    }
+
+    public boolean istDiplomacia() {
+        return tDiplomacia;
+    }
+
+    public void setDiplomacia(boolean diplomacia) {
+        tDiplomacia = diplomacia;
+    }
+
+    public boolean istEnganação() {
+        return tEnganação;
+    }
+
+    public void setEnganacao(boolean enganacao) {
+        tEnganação = enganacao;
+    }
+
+    public boolean istFortitude() {
+        return tFortitude;
+    }
+
+    public void setFortitude(boolean fortitude) {
+        tFortitude = fortitude;
+    }
+
+    public boolean istFurtividade() {
+        return tFurtividade;
+    }
+
+    public void setFurtividade(boolean furtividade) {
+        tFurtividade = furtividade;
+    }
+
+    public boolean istGuerra() {
+        return tGuerra;
+    }
+
+    public void setGuerra(boolean guerra) {
+        tGuerra = guerra;
+    }
+
+    public boolean istIniciativa() {
+        return tIniciativa;
+    }
+
+    public void setIniciativa(boolean iniciativa) {
+        tIniciativa = iniciativa;
+    }
+
+    public boolean istIntimidacao() {
+        return tIntimidacao;
+    }
+
+    public void setIntimidacao(boolean intimidacao) {
+        tIntimidacao = intimidacao;
+    }
+
+    public boolean istIntuicao() {
+        return tIntuicao;
+    }
+
+    public void setIntuicao(boolean intuicao) {
+        tIntuicao = intuicao;
+    }
+
+    public boolean istInvestigacao() {
+        return tInvestigacao;
+    }
+
+    public void setInvestigacao(boolean investigacao) {
+        tInvestigacao = investigacao;
+    }
+
+    public boolean istJogatina() {
+        return tJogatina;
+    }
+
+    public void setJogatina(boolean jogatina) {
+        tJogatina = jogatina;
+    }
+
+    public boolean istLadinagem() {
+        return tLadinagem;
+    }
+
+    public void setLadinagem(boolean ladinagem) {
+        tLadinagem = ladinagem;
+    }
+
+    public boolean istLuta() {
+        return tLuta;
+    }
+
+    public void setLuta(boolean luta) {
+        tLuta = luta;
+    }
+
+    public boolean istMisticismo() {
+        return tMisticismo;
+    }
+
+    public void setMisticismo(boolean misticismo) {
+        tMisticismo = misticismo;
+    }
+
+    public boolean istNobreza() {
+        return tNobreza;
+    }
+
+    public void setNobreza(boolean nobreza) {
+        tNobreza = nobreza;
+    }
+
+    public boolean istOficio() {
+        return tOficio;
+    }
+
+    public void setOficio(boolean oficio) {
+        tOficio = oficio;
+    }
+
+    public boolean istPercepcao() {
+        return tPercepcao;
+    }
+
+    public void settPercepcao(boolean tPercepcao) {
+        this.tPercepcao = tPercepcao;
+    }
+
+    public boolean istPilotagem() {
+        return tPilotagem;
+    }
+
+    public void setPilotagem(boolean pilotagem) {
+        tPilotagem = pilotagem;
+    }
+
+    public boolean istPontaria() {
+        return tPontaria;
+    }
+
+    public void setPontaria(boolean pontaria) {
+        tPontaria = pontaria;
+    }
+
+    public boolean istReflexos() {
+        return tReflexos;
+    }
+
+    public void setReflexos(boolean reflexos) {
+        tReflexos = reflexos;
+    }
+
+    public boolean istReligiao() {
+        return tReligiao;
+    }
+
+    public void settReligiao(boolean tReligiao) {
+        this.tReligiao = tReligiao;
+    }
+
+    public boolean istSobrevivencia() {
+        return tSobrevivencia;
+    }
+
+    public void setSobrevivencia(boolean sobrevivencia) {
+        tSobrevivencia = sobrevivencia;
+    }
+
+    public boolean istVontade() {
+        return tVontade;
+    }
+
+    public void setVontade(boolean vontade) {
+        tVontade = vontade;
+    }
+
+ */
 }
